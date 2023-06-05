eab35f86e3a31bd91bc8260f216346bf7f0bb866
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-06-05 12:43:24 +0330

    Combine Client-Side and Server-Side Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -2,61 +2,38 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import TodoList from '../components/Todos/TodoList';
 import TodoForm from '../components/Todos/AddNewTodo';
-// import useSWR, { mutate } from 'swr';
+import Todo from '../server/models/todo';
 
-// const fetcher = async () => {
-//   const { data } = await axios.get('/api/todos');
-//   return data;
-// };
-
-export default function Home() {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState(null);
+export default function Home({ todos }) {
+  const [data, setData] = useState(todos);
   const [isShow, setIsShow] = useState(false);
-
-  useEffect(() => {
-    axios
-      .get('/api/todos')
-      .then(({ data }) => {
-        setData(data.todos);
-        setLoading(false);
-      })
-      .catch((error) => console.log(error));
-  }, []);
 
   const deleteTodo = (id) => {
     axios.delete(`/api/todos/${id}`).then(({ data }) => {
-      console.log(data);
       setData(data.todos);
-      setLoading(false);
     });
   };
 
   const addTodo = (e, formData) => {
     e.preventDefault();
-
-    console.log(formData);
-
     axios.post(`/api/todos`, { formData }).then(({ data }) => {
-      console.log(data);
       setData(data.todos);
-      setLoading(false);
     });
   };
 
-  if (loading)
-    return (
-      <div className='flex h-screen w-full flex-col items-center bg-gray-100 px-8 py-4'>
-        <h1 className='w-full rounded-xl bg-white px-8 py-6 text-center text-lg font-bold shadow-lg lg:w-2/3 lg:text-3xl'>
-          TodoList App using Next.js & TailwindCSS
-        </h1>
-        <div className='mt-4 w-full rounded-xl bg-white p-8 shadow-md lg:w-1/2'>
-          <div className='flex w-full flex-col items-center justify-center gap-y-6 rounded-lg text-2xl font-medium text-slate-800'>
-            Loading ... Please Wait.
-          </div>
-        </div>
-      </div>
-    );
+  // if (loading)
+  //   return (
+  //     <div className='flex h-screen w-full flex-col items-center bg-gray-100 px-8 py-4'>
+  //       <h1 className='w-full rounded-xl bg-white px-8 py-6 text-center text-lg font-bold shadow-lg lg:w-2/3 lg:text-3xl'>
+  //         TodoList App using Next.js & TailwindCSS
+  //       </h1>
+  //       <div className='mt-4 w-full rounded-xl bg-white p-8 shadow-md lg:w-1/2'>
+  //         <div className='flex w-full flex-col items-center justify-center gap-y-6 rounded-lg text-2xl font-medium text-slate-800'>
+  //           Loading ... Please Wait.
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
 
   return (
     <div className='flex h-full w-full flex-col items-center bg-gray-100 px-8 py-4 lg:h-screen lg:max-h-screen lg:overflow-y-hidden'>
@@ -80,4 +57,14 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const todos = await Todo.find({});
+
+  return {
+    props: {
+      todos: JSON.parse(JSON.stringify(todos)),
+    },
+  };
 }
